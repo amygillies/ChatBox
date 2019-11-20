1726b09d201cb9788b9e843387ae1c8e721d6ae3
Amy Gillies <2254790g@student.gla.ac.uk>
2019-11-20 13:53:00 +0000

    Web Socket Implementation

---
+++ src/main/java/com/example/ChatBox/config/ChatBoxConfig.java
@@ -0,0 +1,28 @@
+package com.example.ChatBox.config;
+
+//This configures websocket endpoint and message broker
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
+
+// @EnableWebSocketMessageBroker is used to enable the WebSocket server
+// Implement the WebSocketMessageBrokerConfigurer interface and provide ... 
+// implementation for some of its methods to configure a connection
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class ChatBoxConfig implements WebSocketMessageBrokerConfigurer {
+
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry registry) {
+		registry.setApplicationDestinationPrefixes("/app");
+		registry.enableSimpleBroker("/topic");
+	}
+	
+	// Register a websocket endpoint that the clients use to connect to the websocket server
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		registry.addEndpoint("/ws").withSockJS(); //SockJS is used to enable fallback options for browsers that dont support websocket
+	}
+}
---
+++ src/main/java/com/example/ChatBox/controller/ChatBoxEventListener.java
@@ -0,0 +1,40 @@
+package com.example.ChatBox.controller;
+
+import com.example.ChatBox.model.ChatMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectedEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Component
+public class ChatBoxEventListener {
+	private static final Logger logger = LoggerFactory.getLogger(ChatBoxEventListener.class);
+	
+	@Autowired
+	private SimpMessageSendingOperations messagingTemplate;
+	
+	@EventListener
+	public void handleWebSocketConnectListener(SessionConnectedEvent event) {
+		logger.info("Receiverd a new web socket connection");
+	}
+	
+	@EventListener
+	public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+		StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+		
+		String username = (String) headerAccessor.getSessionAttributes().get("username");
+		if(username != null) {
+			
+			ChatMessage chatMessage = new ChatMessage();
+			chatMessage.setType(ChatMessage.MessageType.LEAVE);
+			chatMessage.setSender(username);
+			
+			messagingTemplate.convertAndSend("/topic/public", chatMessage);
+		}
+	}
+}
