843ecd30b4ec6676e5aeec5fa8ed4152ffde268e
Amy Gillies <2254790g@student.gla.ac.uk>
2019-11-20 13:02:54 +0000

    first commit

---
+++ src/main/java/com/example/ChatBox/ChatBoxApplication.java
@@ -0,0 +1,13 @@
+package com.example.ChatBox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatBoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ChatBox/ChatBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ChatBox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
